1bee9bf59df959a43d32522c66ce2883e95dda60
Votre Nom <Vous@exemple.com>
2019-02-20 16:10:16 +0100

    changes

---
+++ assets/js/javaSc.js
@@ -0,0 +1,32 @@
+/* navbar ma non funziona 
+$(document).on("click", '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+  
+---> date restaurants;
+document.getElementById(ouvert).addEventListener("DOMContentLoaded",function(){
+ /* var date = new Date();
+  var hours = date.getHours();
+  var days = date.getDay(); 
+  var curDate=new Date();
+		document.getElementById("showDate").innerHTML = curDate.toString();
+
+
+})
+document.getElementById(ouvert).innerHTML = Date();
+
+
+document.getElementById(ouvert).
+
+
+var D = new Date();
+
+var month = d.getMonth() + 1;
+var day = d.getDate();
+var year = d.getFullYear();
+var date = month + "/" + day + "/" + year;
+
+document.write("Current date:" + date);
+*/
+
